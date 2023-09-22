9c5a4da1dc893806391aabaaddadc477c827f7b7
Lolly Blackthorn <lorranysantos08@gmail.com>
2023-09-22 16:39:21 -0300

    primeiro commit desafio

---
+++ Contabanco/ContaTerminal.java
@@ -0,0 +1,13 @@
+package Contabanco;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) throws Exception {
+        
+            System.out.println("Primeiro desafio");
+
+
+
+    }
+
+}
