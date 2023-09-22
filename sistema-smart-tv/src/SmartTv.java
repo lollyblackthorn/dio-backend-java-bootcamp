6fe5642697590890eb623f19d86bfed6b1602351
Lolly Blackthorn <lorranysantos08@gmail.com>
2023-09-22 16:27:09 -0300

    atualização

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,6 +3,29 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("Canal atual: " + canal);
+    }
+
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("Canal atual: " + canal);
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume: " + volume);
+    }
+
     public void ligar(){//método
         ligada = true;
     }
